6ca795e69dd571beb7448d0809b30915b0b0e85e
Erick <erick@turn7.io>
2021-09-30 18:03:15 -0700

    project instructions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
-// TODO: import useFormik from formik library
+import {useFormik, validateYupSchema} from 'formik';
 
 function App() {
-  // TODO: add a const called formik assigned to useFormik()
-
+  const formik = useFormik({
+    initialValues:{
+      name:'',
+      email:'',
+      password:''
+    },
+    onSubmit: values => {
+      window.alert("Login Successful");
+    },
+    validate: values => {
+      let errors = {};
+      console.log(values.email.indexOf('@'));
+      if(values.email.indexOf('@') < 0) errors.email = 'Username should be an email';
+      if(!values.password) errors.password = 'Field required';
+      return errors;
+    }    
+  })
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email</div>
+        <input id="emailField" name="email" onChange={formik.handleChange} value={formik.values.email} type="text" />
+        {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div> : null}
+
+        <div>Password</div>
+        <input name="password" id="pswField" onChange={formik.handleChange} value={formik.values.password} type="password" />
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null}
+        <button type="submit" id="submitBtn">Submit</button>
+      </form>
     </div>
   );
 }
